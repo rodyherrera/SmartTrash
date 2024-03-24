fa55448adfbeb1cb4924080b4dddf6e22fd5d8ad
rodyherrera <contact@rodyherrera.com>
2024-03-24 16:40:06 -0300

    refactor: modularization and readability of the code.

--- Board.ino
+++ Board.ino
@@ -13,83 +13,78 @@
 
 LiquidCrystal_I2C lcd(0x027, 16, 2);
 
-void setup(){
-  lcd.init();
-  lcd.backlight();
-  lcd.setCursor(0, 0);
-  lcd.print("Initializing...");
-  delay(1000);
-  Serial.begin(9600);
-  pinMode(TRIGGER_PIN, OUTPUT);
-  pinMode(ECHO_PIN, INPUT);
-  pinMode(ERROR_LED_PIN, OUTPUT);
-  pinMode(BLUE_LED_PIN, OUTPUT);
-  // Sensor stabilization
-  delay(500);
-}
-
-void loop(){
-  long duration = getDistance();
-  if(duration != -1){
-    displayUsage(duration);
-  }else{
-    displaySensorError();
-  }
-}
-
-// Usage percent from MAX_SENSOR_DISTANCE
-float calculateUsagePercentage(float distance){
-  if(distance > MAX_SENSOR_DISTANCE) return 100.0f;
-  return (distance / MAX_SENSOR_DISTANCE) * 100.0f;
-}
-
-// Calculates distance based on sensor readings
-long getDistance(){
-  digitalWrite(TRIGGER_PIN, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIGGER_PIN, LOW);
-  long duration = pulseIn(ECHO_PIN, HIGH, DISTANCE_READ_TIMEOUT);
-  return (duration == 0) ? -1 : duration;
-}
-
 void printCentered(const char* text, unsigned short int columnIndex = 0){
-  lcd.setCursor((16 - strlen(text)) / 2, columnIndex);
-  lcd.print(text);
+    unsigned short int textLength = strlen(text);
+    unsigned short int position = (16 - textLength) / 2;
+    // Check if text will fit on the LCD
+    if(position >= 0){
+        lcd.setCursor(position, columnIndex);
+        lcd.print(text);
+    }
 };
 
-// Displays usage on the LCD
 void displayUsage(long duration){
-  float distance = duration * 0.034 / 2;
-  lcd.clear();
+    float distance = duration * 0.034 / 2;
+    lcd.clear();
+    printCentered("Usage");
 
-  printCentered("Usage"); 
-  float usagePercentage = calculateUsagePercentage(distance);
+    float usagePercentage = calculateUsagePercentage(distance);
+    char displayStringBuffer[30];
+    sprintf(displayStringBuffer, "%d%% - %d cm", (int)usagePercentage, (int)distance);
+    printCentered(displayStringBuffer, 1);
 
-  char displayString[30];
-  sprintf(displayString, "%d%% - %d cm", (int)usagePercentage, (int)distance);
-  printCentered(displayString, 1); 
-
-  if(usagePercentage == 100.0f){
-    digitalWrite(ERROR_LED_PIN, HIGH);
-    delay(500);
-  }else{
-    digitalWrite(ERROR_LED_PIN, LOW); 
-    digitalWrite(BLUE_LED_PIN, HIGH);
+    const bool isPercentageCorrect = usagePercentage < 100.0f;
+    digitalWrite(ERROR_LED_PIN, !isPercentageCorrect);
+    digitalWrite(BLUE_LED_PIN, isPercentageCorrect);
     delay(500);
     digitalWrite(BLUE_LED_PIN, LOW);
-  }
-}
+};
 
-// Displays a sensor error message on the LCD
 void displaySensorError(){
-  lcd.clear();
-  printCentered("Sensor Error!");
-  printCentered("Check connection.", 1);
+    lcd.clear();
+    printCentered("Sensor Error,");
+    printCentered("Nothing to do.", 1);
+    
+    for(int i = 0; i < ERROR_BLINK_COUNT; i++){
+        digitalWrite(ERROR_LED_PIN, HIGH);
+        delay(ERROR_BLINK_DELAY_MS);
+        digitalWrite(ERROR_LED_PIN, LOW);
+        delay(ERROR_BLINK_DELAY_MS);
+    }
+};
 
-  for(int i = 0; i < ERROR_BLINK_COUNT; i++){
-    digitalWrite(ERROR_LED_PIN, HIGH);
-    delay(ERROR_BLINK_DELAY_MS);
-    digitalWrite(ERROR_LED_PIN, LOW);
-    delay(ERROR_BLINK_DELAY_MS);
-  }
-}
+float calculateUsagePercentage(float distance){
+    distance = min(distance, MAX_SENSOR_DISTANCE);
+    return (distance / MAX_SENSOR_DISTANCE) * 100.0f;
+};
+
+long getDistance(){
+    digitalWrite(TRIGGER_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIGGER_PIN, LOW);
+    
+    long duration = pulseIn(ECHO_PIN, HIGH, DISTANCE_READ_TIMEOUT);
+    return (duration <= 0) ? (-1) : (duration);
+};
+
+void setup(){
+    lcd.init();
+    lcd.backlight();
+    
+    Serial.begin(9600);
+
+    pinMode(TRIGGER_PIN, OUTPUT);
+    pinMode(ECHO_PIN, INPUT);
+    pinMode(ERROR_LED_PIN, OUTPUT);
+    pinMode(BLUE_LED_PIN, OUTPUT);
+};
+
+void loop(){
+    long duration = getDistance();
+
+    if(duration == -1){
+        displaySensorError();
+    }else{
+        displayUsage(duration);
+    }
+};
