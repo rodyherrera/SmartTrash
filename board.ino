770746ecc91ccaa176abf12e2f9e73310733a040
rodyherrera <contact@rodyherrera.com>
2024-03-26 21:15:24 -0300

    chore: updated .gitignore file & board.ino refactor.

--- board.ino
+++ board.ino
@@ -22,8 +22,9 @@ const unsigned short int BLUE_PIN = D3;
 // This constant is used for distance calculations in sensor measurements.
 const float SPEED_OF_SOUND_CM_PER_US = 0.034 / 2;
 
-unsigned long lasTime = 0;
-unsigned short int timerDelay = 3000;
+const unsigned int MEASUREMENT_DELAY_MS = 3000;  
+
+unsigned long lastTime = 0;
 
 static unsigned short int getDistance(){
     // Send a brief high pulse to trigger the sensor
@@ -35,8 +36,33 @@ static unsigned short int getDistance(){
     
     // Measure round-trip echo time
     unsigned long duration = pulseIn(ECHO_PIN, HIGH, DISTANCE_READ_TIMEOUT);
-    const unsigned short int distance = duration * SPEED_OF_SOUND_CM_PER_US;
-    return distance;
+    return duration * SPEED_OF_SOUND_CM_PER_US;
+};
+
+// Sends distance data to the server
+void sendData(unsigned short int distance){
+    if(WiFi.status() != WL_CONNECTED){
+        Serial.println("WiFi not connected.");
+        return;
+    }
+    WifiClient client;
+    HTTPClient http;
+    DynamicJsonDocument body(100);
+    body["measuredDistance"] = distance;
+    String jsonPayload;
+    serializeJson(body, jsonPayload);
+    if(!http.begin(client, SERVER_ENDPOINT)){
+        Serial.println("Error connecting to the server.");
+        return;
+    }
+    http.addHeader("Content-Type". "application/json");
+    unsigned short int httpResponseCode = http.POST(jsonPayload);
+    if(httpResponseCode == HTTP_CODE_OK){
+        Serial.println("Data sent successfully.");
+    }else{
+        Serial.print("Error sending data. HTTP code: ");
+        Serial.println(httpResponseCode);
+    }
 };
 
 void setup(){
@@ -63,36 +89,11 @@ void setup(){
 
 void loop(){
     digitalWrite(GREEN_PIN, HIGH);
-    if((millis() - lasTime) > timerDelay){
-        // Check WiFi connection status
-        if(WiFi.status() == WL_CONNECTED){
-            WiFiClient client;
-            HTTPClient http;
-
-            const unsigned short int distance = getDistance();
-
-            DynamicJsonDocument body(100);
-            body["measuredDistance"] = distance;
-
-            String jsonPayload;
-            serializeJson(body, jsonPayload);
-
-            if(http.begin(client, SERVER_ENDPOINT)){
-                http.addHeader("Content-Type", "application/json");
-                unsigned short int httpResponseCode = http.POST(jsonPayload);
-                if(httpResponseCode == HTTP_CODE_OK){
-                    Serial.println("Data sent successfully.");
-                }else{
-                    Serial.println("Error sending data");
-                }
-                http.end();
-            }else{
-                Serial.println("Error connecting to the server.");
-            }
-        }else{
-            Serial.println("WiFi not connected");
-        }
-        lasTime = millis();
+    unsigned long currentTime = millis();
+    if(currentTime - lastTime > MEASUREMENT_DELAY_MS){
+        unsigned short int distance = getDistance();
+        sendData(distance);
+        lastTime = currentTime;
     }
     digitalWrite(GREEN_PIN, LOW);
 };
