f92338b3ff8141d0285614d6cf7013c992a639b9
rodyherrera <contact@rodyherrera.com>
2024-03-30 01:57:10 -0300

    feat: footer container implementation.

---
+++ board/admin-portal/src/components/general/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+
+    return (
+        <footer className='Footer-Container'>
+            <section className='Footer-Copyright-Container'>
+            </section>
+        </footer>
+    );
+};
+
+export default Footer;
--- board/admin-portal/src/components/general/Layout.jsx
+++ board/admin-portal/src/components/general/Layout.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '@components/general/Header';
+import Footer from '@components/general/Footer';
 import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
@@ -8,6 +9,7 @@ const Layout = () => {
         <React.Fragment>
             <Header />
             <Outlet />
+            <Footer />
         </React.Fragment>
     );
 };
