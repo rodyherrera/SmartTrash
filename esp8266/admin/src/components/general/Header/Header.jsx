d579ef0db3edcffc95e7cd0125ffc5cba9d0bd6f
rodyherrera <contact@rodyherrera.com>
2024-04-07 04:16:38 -0400

    feat: fonts added to assets & UI changes.

--- esp8266/admin/src/components/general/Header/Header.jsx
+++ esp8266/admin/src/components/general/Header/Header.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import CurrentWiFiConnection from '@components/network/CurrentWiFiConnection';
 import RestartESPButton from '@components/server/RestartESPButton';
 import ResetAPConfigButton from '@components/server/ResetAPConfigButton';
+import { useNavigate } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
+    const navigate = useNavigate();
 
     return (
         <header className='Header-Container'>
-            <section className='Header-Brand-Container'>
+            <section className='Header-Brand-Container' onClick={() => navigate('/')}>
                 <h3 className='Header-Brand-Title'>ST</h3>
             </section>
             <section className='Header-ESP-Related-Container'>
