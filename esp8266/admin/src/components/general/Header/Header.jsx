94b84c3314433a4d5a727479d677393bfd3fc6cb
rodyherrera <contact@rodyherrera.com>
2024-04-08 09:01:16 -0400

    chore: adjustments to the height of the main container and style of buttons in the header.

--- esp8266/admin/src/components/general/Header/Header.jsx
+++ esp8266/admin/src/components/general/Header/Header.jsx
@@ -10,8 +10,10 @@ const Header = () => {
 
     return (
         <header className='Header-Container'>
-            <section className='Header-Brand-Container' onClick={() => navigate('/')}>
-                <h3 className='Header-Brand-Title'>ST</h3>
+            <section className='Header-Brand-Related-Container'>
+                <article className='Header-Brand-Container' onClick={() => navigate('/')}>
+                    <h3 className='Header-Brand-Title'>ST</h3>
+                </article>
             </section>
             <section className='Header-ESP-Related-Container'>
                 <ResetAPConfigButton />
