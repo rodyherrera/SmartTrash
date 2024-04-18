3ba94288270b7fbb6e17a51c7684feb5901c8427
rodyherrera <contact@rodyherrera.com>
2024-04-18 18:41:13 -0400

    chore: changes to the MQTTConnector class and the way it is sent distance.

--- server/utilities/mqttConnector.js
+++ server/utilities/mqttConnector.js
@@ -3,98 +3,43 @@ const Device = require('@models/device');
 const DeviceLog = require('@models/deviceLog');
 
 /**
- * This class manages the connection to an MQTT server, receives messages from
- * devices, and updates device logs in the database.
+ * Manages communication with the MQTT server, handling messages,
+ * and updating device data.
 */
-class mqttController{
+class MQTTController{
     /**
-     * The constructor initializes the MQTT client and an update queue.
+     * Creates an MQTTController instance.
     */
     constructor(){
+        /**
+         * The active MQTT client connection.
+         * @type {mqtt.MqttClient}
+        */
         this.client = null;
+        /**
+         * A map of registered message handlers and their options.
+         * @type {Map<string, {options: Object, callback: function}>} 
+        */
         this.handlers = new Map();
     };
 
-    addHandler(id, options, callback){
-        this.handlers.set(id, { options, callback });
-    };
-
-    deleteHandler(id){
-        this.handlers.delete(id);
-    };
-
     /**
-     * Handles incoming messages from the MQTT server. Expects the message to be
-     * JSON with a 'measuredDistance' property, then updates a device record with 
-     * the distance information.
-     *
-     * @param {string} topicName - The topic name where the message originated.
-     * @param {Buffer} message - The message in binary format.
-     * @throws {Error} - If there's an error parsing the JSON message.
+     * Handles an incoming MQTT message, updates the relevant device, and logs data.
+     * @param {string} topicName - The MQTT topic name.
+     * @param {number} distance - The measured distance reported by the device.
+     * @returns {Promise<void>}
     */
-    async messageEventHandler(topicName, message){
+    async handleIncomingMessage(topicName, distance){
         try{
-            const { data } = JSON.parse(message.toString());
-            const { measuredDistance } = data;
-            const stduid = topicName.toString();
-            const device = await Device
-                .findOne({ stduid })
-                .select('height');
-                
-            const usagePercentage = Math.round((measuredDistance / device.height) * 100);
-            device.usagePercentage = usagePercentage;
-            for(const { options, callback } of this.handlers.values()){
-                if(options?.topicName && (options.topicName !== topicName)){
-                    continue;
-                }
-                callback({ measuredDistance, usagePercentage });
-            }
-            await DeviceLog.create({ 
-                height: device.height, 
-                distance: measuredDistance, 
-                usagePercentage, 
-                stduid 
-            });
-            await device.save();
+            await this.updateDeviceAndLog(topicName, distance.toString());
         }catch(error){
-            console.error(`[Quantum Cloud Server]: Error parsing message: ${error}`);
+            console.error(`[SmartTrash Cloud Server]: Error parsing message: ${error}`);
         }
     };
 
     /**
-     * Checks for recent activity on an MQTT topic. Subscribes to the topic
-     * and waits for a message.
-     * 
-     * @param {string} topicName - The name of the MQTT topic to subscribe to.
-     * @param {number} [timeout=3000] - Milliseconds to wait before assuming no activity.
-     * @returns {Promise<boolean>} - Resolves to `true` if a message arrives within
-     *                              the timeout, otherwise resolves to `false`.
-     * @throws {Error} - If the MQTT client is not connected (Error code 'MQTT::ClientNotConnected').
-    */
-    async checkTopicActivity(topicName, timeout = 3000){
-        return new Promise(async (resolve, reject) => {
-            if(!this.client || !this.client.connected){
-                reject(new Error('MQTT::ClientNotConnected'));
-                return;
-            }
-            let messageReceived = false;
-            await this.client.subscribeAsync(topicName);
-            const messageHandler = () => {
-                messageReceived = true;
-            };
-            this.client.on('message', messageHandler);
-            setTimeout(async () => {
-                await this.client.unsubscribeAsync(topicName);
-                this.client.off('message', messageHandler);
-                resolve(messageReceived);
-            }, timeout);
-        });
-    };
-
-    /**
-     * Connects to the MQTT server, subscribes to message topics, and starts 
-     * update processing. Registers events to handle incoming messages.
-     * @throws {Error} - If an error occurs while connecting to the MQTT server.
+     * Establishes a connection to the MQTT server.
+     * @returns {Promise<void>}
     */
     async connect(){
         try{
@@ -102,14 +47,91 @@ class mqttController{
                 username: process.env.MQTT_USERNAME,
                 password: process.env.MQTT_PASSWORD
             });
-            this.client.on('message', this.messageEventHandler.bind(this));
+            this.client.on('message', this.handleIncomingMessage.bind(this));
             console.log('[SmartTrash Cloud Server]: Successfully connected to MQTT Server.');
         }catch(error){
-            console.log('[SmartTrash Cloud Server]: Error trying connect to MQTT Server ->', error);
+            console.error('[SmartTrash Cloud Server]: Error connecting to MQTT Server:', error);
         }
+    };
+
+    /**
+     * Verifies activity on a specified MQTT topic.
+     * @param {string} topicName - The name of the MQTT topic to monitor.
+     * @param {number} [timeout=3000] - Timeout in milliseconds.
+     * @returns {Promise<boolean>} - `true` if a message was received, `false` otherwise.
+     * @throws {Error} - If the client is not connected (Error code: 'MQTT::ClientNotConnected').
+    */  
+    async checkTopicActivity(topicName, timeout = 3000){
+        if(!this.client || !this.client.connected){
+            throw new Error('MQTT::ClientNotConnected'); 
+        }
+        return new Promise((resolve, reject) => {
+            let messageReceived = false;
+            const messageHandler = () => {
+                messageReceived = true;
+                unsubscribeAndCleanup();
+            };
+
+            const unsubscribeAndCleanup = async () => {
+                await this.client.unsubscribeAsync(topicName);
+                this.client.off('message', messageHandler);
+                resolve(messageReceived);
+            };
+
+            this.client.on('message', messageHandler);
+            this.client.subscribeAsync(topicName).catch(reject);
+            setTimeout(unsubscribeAndCleanup, timeout);
+        });
+    };
+
+    /**
+     * Updates device data and creates a log entry based on measured distance.
+     * @param {string} topicName - The MQTT topic name.
+     * @param {string} measuredDistance - The distance measured by the device.
+     * @returns {Promise<void>}
+    */
+    async updateDeviceAndLog(topicName, measuredDistance){
+        const stduid = topicName.toString();
+        const device = await Device.findOne({ stduid }).select('height');
+        const usagePercentage = Math.round((measuredDistance / device.height) * 100);
+        device.usagePercentage = usagePercentage;
+        // Notify handlers
+        for(const { options, callback } of this.handlers.values()){
+            if(options?.topicName && (options.topicName !== topicName)){
+                continue;
+            }
+            callback({ measuredDistance, usagePercentage });
+        }
+        await Promise.all([
+            DeviceLog.create({
+                height: device.height,
+                distance: measuredDistance,
+                usagePercentage,
+                stduid
+            }),
+            device.save()
+        ]);
+    };
+
+    /**
+     * Adds a message handler for MQTT messages.
+     * @param {string} id - A unique identifier for the handler.
+     * @param {Object} options - Handler options (e.g., topicName).
+     * @param {function} callback - Callback function to execute when a message is received.
+    */
+    addHandler(id, options, callback){
+        this.handlers.set(id, { options, callback });
+    };
+
+    /**
+     * Removes a message handler.
+     * @param {string} id - The ID of the handler to remove.
+    */
+    deleteHandler(id){
+        this.handlers.delete(id);
     }
 };
 
-const mqttClient = new mqttController();
+const mqttClient = new MQTTController();
 
 module.exports = mqttClient;
