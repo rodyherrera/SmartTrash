b220e23ec02978f9cde27b8266d3350e71a0548a
rodyherrera <contact@rodyherrera.com>
2024-04-27 20:24:49 -0400

    doc: @queues/device.js & @queues/deviceLog.js

--- server/queues/device.js
+++ server/queues/device.js
@@ -1,6 +1,25 @@
 const DeviceLog = require('@models/deviceLog');
 const Bull = require('bull');
 
+/**
+ * Creates a Bull queue named 'deviceQueue'. The queue configuration
+ * uses environment variables for Redis connection details (host, port, password).
+ *
+ * The expected format of `process.env` is:
+ *
+ * ```
+ * process.env = {
+ *   REDIS_HOST: 'your_redis_host',
+ *   REDIS_PORT: 1234,  // Replace with your Redis port
+ *   REDIS_PASSWORD: 'your_redis_password' // Optional password
+ * };
+ * ```
+ *
+ * @typedef {Bull.Queue} DeviceQueue
+ * @property {async function(JobData): Promise<void>} process - Processes messages (jobs) in the queue.
+ *
+ * @module deviceQueueProcessor
+*/
 const deviceQueue = new Bull('deviceQueue', {
     redis: {
         host: process.env.REDIS_HOST,
@@ -9,6 +28,14 @@ const deviceQueue = new Bull('deviceQueue', {
     }
 });
 
+/**
+ * A worker function that processes messages (jobs) from the `deviceQueue`.
+ *
+ * @param {Job} job - The message (job) containing the data for creating a device log.
+ * @param {function(Error?): void} done - A callback function to signal job completion.
+ *
+ * @module deviceQueueProcessor
+*/
 deviceQueue.process(async function(job, done){
     try{
         const { data } = job;
@@ -20,4 +47,9 @@ deviceQueue.process(async function(job, done){
     }
 });
 
+/**
+ * Exports the `deviceQueue` instance, making it available to other parts of the application.
+ *
+ * @module deviceQueueProcessor
+*/
 module.exports = deviceQueue;
--- server/queues/deviceLog.js
+++ server/queues/deviceLog.js
@@ -2,6 +2,25 @@ const Bull = require('bull');
 const Device = require('@models/device');
 const DeviceLogPartition = require('@models/deviceLogPartition');
 
+/**
+ * Creates a Bull queue named 'deviceLog'. The queue configuration
+ * uses environment variables for Redis connection details (host, port, password).
+ *
+ * The expected format of `process.env` is:
+ *
+ * ```
+ * process.env = {
+ *   REDIS_HOST: 'your_redis_host',
+ *   REDIS_PORT: 1234,  // Replace with your Redis port
+ *   REDIS_PASSWORD: 'your_redis_password' // Optional password
+ * };
+ * ```
+ *
+ * @typedef {Bull.Queue} DeviceLogQueue
+ * @property {async function(JobData): Promise<void>} process - Processes messages (jobs) in the queue.
+ *
+ * @module deviceLogQueueProcessor
+*/
 const deviceLogQueue = new Bull('deviceLog', {
     redis: {
         host: process.env.REDIS_HOST,
@@ -10,11 +29,27 @@ const deviceLogQueue = new Bull('deviceLog', {
     }
 });
 
+/**
+ * Generates a name for the device log partition based on the creation date.
+ *
+ * @param {Date} creationDate - The date the device log was created.
+ * @returns {string} - The formatted date string representing the partition name (YYYY-MM-DD).
+ *
+ * @module deviceLogQueueProcessor
+*/
 const getLogPartitionName = (creationDate) => {
     const formattedDate = new Date(creationDate).toISOString().slice(0, 10);
     return formattedDate;
 };
 
+/**
+ * A worker function that processes messages (jobs) from the `deviceLogQueue`.
+ *
+ * @param {Job} job - The message (job) containing the device log data.
+ * @param {function(Error?): void} done - A callback function to signal job completion.
+ *
+ * @module deviceLogQueueProcessor
+*/
 deviceLogQueue.process(async function(job, done){
     try{
         const { data } = job;
@@ -34,4 +69,9 @@ deviceLogQueue.process(async function(job, done){
     }
 });
 
+/**
+ * Exports the `deviceLogQueue` instance, making it available to other parts of the application.
+ *
+ * @module deviceLogQueueProcessor
+*/
 module.exports = deviceLogQueue;
